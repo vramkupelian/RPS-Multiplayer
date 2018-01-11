f28184baf9f831d2788267fe2ee5d3b10cafa803
unknown <vramkupelian@gmail.com>
2018-01-10 18:05:03 -0800

    Update 1.1 Firebase adding names. Added basic chat storing localy: needs to be put into firebase.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,18 +1,75 @@
-    var user1 = "";
-    var user2 = "";
+    database.ref().get({
+        player1,
+        player2,
+    });
+
+
+    console.log($(".p2-name").text().length);
+    $(".add-name").on("click", function(event){
+        //prevents refresh on form submission
+        event.preventDefault();
+
+        //grabs user chat input
+        var userName = $(".name").val().trim();
+
+        //Create a new variable that will hold a <p>
+        var nameItem = $("<p>");
+        nameItem.addClass("player");
+
+        
+        nameItem.append(userName);
+
+       
+        //If there is no final player, add name
+        if(!$("p").hasClass("final-player")){
+            if($("p").hasClass("player")){
+                $(".p2-name").append(nameItem);
+                $("p").addClass("final-player");
+
+                database.ref().update({
+                    player2: userName,
+                    p2Wins: 0,
+                    p2Losses: 0,
+                });
+
+            }
+            else{
+                $(".p1-name").append(nameItem);
+                
+                database.ref().set({
+                    player1: userName,
+                    p1Wins: 0,
+                    p1Losses: 0,
+                });
+            }
+        }
+        else{
+            $(".name").val("");
+            return;
+        }
+
+        //Clear textbox when done
+        $(".name").val("");
     
+    });
+
+    
+
+
+
+
+
     $(".add-chat").on("click", function(event){
         //prevents refresh on form submission
         event.preventDefault();
 
         //grabs user chat input
         var userChat = $(".chat").val().trim();
-        console.log(userChat);
 
         //Create a new variable that will hold a <p>
         var chatItem = $("<p>");
 
-        chatItem.attr("player-name", "User: " + user1);
+        
         chatItem.append(userChat);
 
         //Add <p> to chat-log
@@ -20,5 +77,7 @@
 
         //Clear textbox when done
         $(".chat").val("");
-        console.log("Made to end of function");
+    
     });
+
+    
