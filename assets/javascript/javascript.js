3c8b5bdbc0090e86c7dbf1a5f3d451356c991aa0
unknown <vramkupelian@gmail.com>
2018-01-12 14:43:06 -0800

    Update 1.2 Firebase working for player stats. Rewrote logic for limiting to 2 players

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,35 +1,38 @@
-
 var config = {
-            apiKey: "AIzaSyCAIt42NvpFwzSNogdJvFTMUeHNf-jPxWY",
-            authDomain: "rock-paper-scissors-mp-79a33.firebaseapp.com",
-            databaseURL: "https://rock-paper-scissors-mp-79a33.firebaseio.com",
-            projectId: "rock-paper-scissors-mp-79a33",
-            storageBucket: "",
-            messagingSenderId: "153941745547"
+    apiKey: "AIzaSyCAIt42NvpFwzSNogdJvFTMUeHNf-jPxWY",
+    authDomain: "rock-paper-scissors-mp-79a33.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-mp-79a33.firebaseio.com",
+    projectId: "rock-paper-scissors-mp-79a33",
+    storageBucket: "",
+    messagingSenderId: "153941745547"
 };
+
 firebase.initializeApp(config);
    
 var database = firebase.database();
+// var connections = database.ref("connections");
+// console.log(connections);
 
-
-database.ref().on("value" , function(snapshot){
-
-
-});
+// database.ref().on("value" , function(snapshot){
+// // user1 = snapshot.val().
+// });
 
 // var user1 ={
 //     name:"",
 //     wins: 0,
 //     losses: 0,
+//     choice: "",
 // }
 
 // var user2 ={
 //     name:"",
 //     wins: 0,
-//     losses,
+//     losses:0,
+//     choice:"",
 // }
 
-console.log($(".p2-name").text().length);
+
+//Adding names, only 2 people
 $(".add-name").on("click", function(event){
         //prevents refresh on form submission
         event.preventDefault();
@@ -40,45 +43,55 @@ $(".add-name").on("click", function(event){
         //Create a new variable that will hold a <p>
         var nameItem = $("<p>");
         nameItem.addClass("player");
-
         
         nameItem.append(userName);
 
-       
         //If there is no final player, add name
-        if(!$("p").hasClass("final-player")){
-            if($("p").hasClass("player")){
-                $(".p2-name").append(nameItem);
-                $("p").addClass("final-player");
+        // if(!$("p").hasClass("final-player")){
+        if($(".p1-name").is(":empty")){
+            console.log("P1 is empty,lets add");
+            // if($("p").hasClass("player")){
+            //     $(".p2-name").append(nameItem);
+            //     $("p").addClass("final-player");
+            //     user2 = userName;
 
-                database.ref().update({
-                    player2: userName,
-                    p2Wins: 0,
-                    p2Losses: 0,
-                });
+            //     database.ref().push({
+            //         user: userName,
+            //         wins:0,
+            //         losses: 0,
+            //         choice:"",
+            //     });
+            
+            // user1 = userName;
+            database.ref().push({
+                user: userName,
+                wins: 0,
+                losses: 0,
+                choice:"",
+            });
 
             }
-            else{
-                $(".p1-name").append(nameItem);
+            else if($(".p2-name").is(":empty")){
                 
-                database.ref().update({
-                    player1: userName,
-                    p1Wins: 0,
-                    p1Losses: 0,
+                // user1 = userName;
+                database.ref().push({
+                    user: userName,
+                    wins: 0,
+                    losses: 0,
+                    choice:"",
                 });
             }
-        }
-        else{
-            $(".name").val("");
-            return;
-        }
+        // }
+        // else{
+        //     $(".name").val("");
+        //     return;
+        // }
 
         //Clear textbox when done
-        $(".name").val("");
-    
+        $(".name").val("");   
 });
 
-
+//chat
     $(".add-chat").on("click", function(event){
         //prevents refresh on form submission
         event.preventDefault();
@@ -88,7 +101,6 @@ $(".add-name").on("click", function(event){
 
         //Create a new variable that will hold a <p>
         var chatItem = $("<p>");
-
         
         chatItem.append(userChat);
 
@@ -98,10 +110,26 @@ $(".add-name").on("click", function(event){
         //Clear textbox when done
         $(".chat").val("");
 
-        database.ref().update({
-            chat1: userChat,
+        database.ref().push({
+            chat: userChat,
         });
     
+    });
+
+    //retrieve from database
+    database.ref().on("child_added", function(snapshot){
+
+        var newName = $("<p>").text("Player: " + snapshot.val().user);
+        var winCount = $("<p>").text("Wins: " + snapshot.val().wins);
+        var lossCount = $("<p>").text("Losses: " + snapshot.val().losses);
+
+        if($(".p1-name").is(":empty")){
+                   $(".p1-name").append(newName).append(winCount).append(lossCount);
+ 
+        }
+        else{
+            $(".p2-name").append(newName).append(winCount).append(lossCount);
+        }
     });
 
     
